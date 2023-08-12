92fa7a6f8038b9a42ae230efeaccdb9d2a7cda3e
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-12 10:40:39 -0400

    Link Component

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
-  return <h1>Register Page</h1>;
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <Link to="/login">Login page</Link>
+      <a href="https://sasnarinedeodat-portfolio.netlify.app/">My Portfolio</a>
+    </div>
+  );
 };
 export default Register;
