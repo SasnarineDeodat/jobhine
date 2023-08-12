8df2eec92d3f3617dfd6bd5c6c2db510b048d50e
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-12 14:02:08 -0400

    Register Page - Setup

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,37 @@
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { Logo } from "../components";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/login">Login page</Link>
-      <a href="https://sasnarinedeodat-portfolio.netlify.app/">My Portfolio</a>
-    </div>
+    <Wrapper>
+      <div className="form">
+        <Logo />
+        <h4>Register</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-input"
+            defaultValue="sasnarine"
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a member?{" "}
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </div>
+    </Wrapper>
   );
 };
 export default Register;
