cf1d7acaa2f992ed311fdcf226c0f1fa5a56899c
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-12 14:12:37 -0400

    FormRow Component

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
-import { Logo } from "../components";
+import { FormRow, Logo } from "../components";
 
 const Register = () => {
   return (
@@ -8,19 +8,20 @@ const Register = () => {
       <div className="form">
         <Logo />
         <h4>Register</h4>
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="form-input"
-            defaultValue="sasnarine"
-            required
-          />
-        </div>
+        <FormRow type="text" name="name" defaultValue="sasnarine" />
+
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue="deodat"
+        />
+
+        <FormRow type="text" name="location" defaultValue="waterbury" />
+
+        <FormRow type="email" name="email" defaultValue="sasnarine@gamil.com" />
+
+        <FormRow type="password" name="password" defaultValue="secret123" />
         <button type="submit" className="btn btn-block">
           submit
         </button>
