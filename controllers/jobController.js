11159eb9d04b3920a2b07ea75307c0a3c8718ba4
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-13 13:41:54 -0400

    Create Job Controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,3 +1,5 @@
+import Job from "../models/JobModel.js";
+
 import { nanoid } from "nanoid";
 
 let jobs = [
@@ -5,22 +7,18 @@ let jobs = [
   { id: nanoid(), company: "google", position: "back-end" },
 ];
 
+//Get All Jobs
 export const getAllJobs = async (req, res) => {
   res.status(200).json({ jobs });
 };
 
+//Create Job
 export const createJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    res.status(400).json({ msg: "please provide company and position" });
-    return;
-  }
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
-  res.status(201).json({ jobs });
+  const job = await Job.create(req.body);
+  res.status(201).json({ job });
 };
 
+// Get Job
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = jobs.find((job) => job.id === id);
@@ -31,6 +29,7 @@ export const getJob = async (req, res) => {
   res.status(200).json({ job });
 };
 
+// Update Job
 export const updateJob = async (req, res) => {
   const { id } = req.params;
   const { company, position } = req.body;
@@ -48,6 +47,7 @@ export const updateJob = async (req, res) => {
   res.status(200).json({ msg: "job modified", job });
 };
 
+// EditJob
 export const editJob = async (req, res) => {
   const { company, position } = req.body;
   if (!company || !position) {
@@ -65,6 +65,7 @@ export const editJob = async (req, res) => {
   res.status(200).json({ msg: "job modified", job });
 };
 
+// Delete Job
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
   const job = jobs.find((job) => job.id === id);
