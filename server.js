14a406ae0a51e451394ad87d547bff14b10db306
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-13 10:39:52 -0400

    Morgan and Dotenv

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 const app = express();
+import morgan from "morgan";
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
 
@@ -12,6 +19,7 @@ app.post("/", (req, res) => {
   res.json({ message: "data received", data: req.body });
 });
 
-app.listen(5100, () => {
-  console.log("server running...");
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+  console.log(`server running on Port ${port}...`);
 });
