bc023966bc34c872e173911bc944c57928df7355
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-14 13:44:01 -0400

    Update User

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -4,10 +4,12 @@ import {
   getCurrentUser,
   updateUser,
 } from "../controllers/userController.js";
+import { validateUpdateUserInput } from "../middleware/validationMiddleware.js";
+
 const router = Router();
 
 router.get("/current-user", getCurrentUser);
 router.get("/admin/app-stats", getApplicationStats);
-router.get("/update-user", updateUser);
+router.patch("/update-user", validateUpdateUserInput, updateUser);
 
 export default router;
