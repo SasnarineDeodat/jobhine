9d5a52d0f147bcfef8566e44230706b7f175bf4b
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-14 18:57:20 -0400

    Get Current User

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -3,18 +3,22 @@ import Wrapper from "../assets/wrappers/Dashboard";
 import { BigSidebar, Navbar, SmallSidebar } from "../components";
 import { useState, createContext, useContext } from "react";
 import { checkDefaultTheme } from "../App";
+import customFetch from "../utils/customFetch";
 
-export const loader = () => {
-  return "hello world";
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get("/users/current-user");
+    return data;
+  } catch (error) {
+    return redirect("/");
+  }
 };
 
 const DashboardContext = createContext();
 
 const DashboardLayout = () => {
-  const data = useLoaderData();
-  console.log(data);
-  // temp
-  const user = { name: "sasnarine" };
+  const { user } = useLoaderData();
+
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
 
@@ -49,7 +53,7 @@ const DashboardLayout = () => {
           <div>
             <Navbar />
             <div className="dashboard-page">
-              <Outlet />
+              <Outlet context={{ user }} />
             </div>
           </div>
         </main>
