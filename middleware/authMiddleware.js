f67160b288a4171b69cad6054231b561ceb04cd9
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-14 14:02:44 -0400

    Get Application Stats

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,7 @@
-import { UnauthenticatedError } from "../errors/customErrors.js";
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+} from "../errors/customErrors.js";
 import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = (req, res, next) => {
@@ -10,6 +13,15 @@ export const authenticateUser = (req, res, next) => {
     req.user = { userId, role };
     next();
   } catch (error) {
-    throw new UnauthenticatedError("authentication invalid");
+    throw new UnauthorizedError("Unauthorized to access this route");
   }
+};
+export const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthorizedError("Unauthorized to access this route");
+    }
+
+    next();
+  };
 };
