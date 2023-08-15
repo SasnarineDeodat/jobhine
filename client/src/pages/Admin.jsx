40c8c3dd35d5efe43d753f4076d01ec15e81e4c0
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-15 15:22:10 -0400

    Admin Page Complete

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -4,6 +4,7 @@ import { useLoaderData, redirect } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import Wrapper from "../assets/wrappers/StatsContainer";
 import { toast } from "react-toastify";
+import StatItem from "../components/StatItem";
 export const loader = async () => {
   try {
     const response = await customFetch.get("/users/admin/app-stats");
@@ -19,7 +20,20 @@ const Admin = () => {
 
   return (
     <Wrapper>
-      <h1>admin page</h1>
+      <StatItem
+        title="current users"
+        count={users}
+        color="#e9b949"
+        bcg="#fcefc7"
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatItem
+        title="total jobs"
+        count={jobs}
+        color="#647acb"
+        bcg="#e0e8f9"
+        icon={<FaCalendarCheck />}
+      />
     </Wrapper>
   );
 };
