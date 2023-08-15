871d72408f0625369a256df5660cb686d8fadfae
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-15 16:22:07 -0400

    Setup Multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,9 +14,10 @@ export const getApplicationStats = async (req, res) => {
   res.status(StatusCodes.OK).json({ users, jobs });
 };
 export const updateUser = async (req, res) => {
+  console.log(req.file);
   const obj = { ...req.body };
   delete obj.password;
-  console.log(obj);
+
   const updateUser = await User.findByIdAndUpdate(req.user.userId, obj);
   res.status(StatusCodes.OK).json({ msg: "update user" });
 };
