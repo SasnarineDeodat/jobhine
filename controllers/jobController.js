90510ecb77d6a3311b71a66c7f9adf6d0116b645
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-15 21:01:48 -0400

    Query Params

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -5,7 +5,11 @@ import day from "dayjs";
 
 //Get All Jobs
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId });
+  console.log(req.query);
+  const jobs = await Job.find({
+    createdBy: req.user.userId,
+    position: req.query.search,
+  });
   res.status(StatusCodes.OK).json({ jobs });
 };
 
